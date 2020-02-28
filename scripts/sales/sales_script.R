de44bcf9e19359b2d73d179da91cb1992b18fc1f
rodserr <rodrigoserranom8@gmail.com>
2020-02-28 16:34:37 -0400

    regcomm

--- scripts/sales/sales_script.R
+++ scripts/sales/sales_script.R
@@ -1,9 +1,11 @@
 # Libaries----
 library(tidyverse)
 library(janitor)
+library(naniar)
 library(wesanderson)
 library(DT)
 library(forecast)
+library(caret)
 
 # Import----
 
@@ -14,6 +16,8 @@ vg <- vg_sales %>%
   select(-one_of(c('global_sales', 'rank'))) %>% 
   pivot_longer(matches('sales'), names_to = 'region', values_to = 'sales') %>% 
   mutate(region = region %>% str_remove('_sales') %>% as.factor())
+
+# Descriptive Plots-----
 
 vg %>% 
   group_by(year, publisher) %>% 
@@ -48,8 +52,8 @@ vg %>%
   theme_minimal() +
   theme(legend.position = "top")
 
-# Top-----
-.top_publisher_n <- vg %>% count(publisher) %>% top_n(10)
+# Top
+.top_publisher_n <- vg_sales %>% count(publisher) %>% top_n(10)
 vg %>% 
   filter(publisher %in% .top_publisher_n$publisher) %>% 
   count(publisher, genre) %>% 
@@ -73,3 +77,71 @@ vg %>%
   geom_col() + 
   coord_flip() +
   scale_fill_manual(values = wes_palette('Darjeeling1', length(unique(vg$genre)), type = 'continuous'))
+
+
+
+# Model Tryouts----
+gg_miss_case(vg_sales)
+gg_miss_upset(vg_sales)
+
+.publisher_count <- vg_sales %>% count(publisher)
+
+table(vg_sales$platform) %>% sort()
+.nintendoplatforms = c("3DS","DS","GB","GBA","N64","GC", "NES","SNES","Wii","WiiU")
+.sonyplatforms = c("PS","PS2","PSP","PS3","PS4","PSV")
+.segaplatforms = c("GEN","SCD","DC","GG","SAT")
+.msplatforms = c("XB","X360", "XOne")
+.otherplatforms = c("2600","3DO","NG","PCFX","TG16","WS")
+
+dataBM <- vg_sales %>% 
+  left_join(.publisher_count, by = 'publisher') %>% 
+  transmute(name, 
+            genre_fct = genre,
+            platform_fct = case_when(platform %in% .nintendoplatforms ~ 'nintendo',
+                                  platform %in% .sonyplatforms ~ 'sony',
+                                  platform %in% .segaplatforms ~ 'sega',
+                                  platform %in% .msplatforms ~ 'ms',
+                                  platform %in% .otherplatforms ~ 'other',
+                                  T ~ 'pc') %>% factor(),
+            publisher_fct = case_when(n < 5 ~ 'small',
+                                   n < 150 ~ 'medium',
+                                   T ~ 'large') %>% factor(),
+            avg_sales = global_sales/(2017-as.numeric(as.character(year)))) %>% 
+  na.omit()
+
+  calc_boxplot_stat <- function(x) {
+    coef <- 1.5
+    n <- sum(!is.na(x))
+    # calculate quantiles
+    stats <- quantile(x, probs = c(0.0, 0.25, 0.5, 0.75, 1.0))
+    names(stats) <- c("ymin", "lower", "middle", "upper", "ymax")
+    iqr <- diff(stats[c(2, 4)])
+    # set whiskers
+    outliers <- x < (stats[2] - coef * iqr) | x > (stats[4] + coef * iqr)
+    if (any(outliers)) {
+      stats[c(1, 5)] <- range(c(stats[2:4], x[!outliers]), na.rm = TRUE)
+    }
+    return(stats)
+  }
+
+dataBM %>% 
+  ggplot(aes(x = platform_fct, y = avg_sales, fill = genre_fct)) +
+  stat_summary(fun.data = calc_boxplot_stat, geom="boxplot", position = "dodge2") +
+  coord_flip() +
+  facet_wrap(~publisher_fct, scales = 'free')
+
+.trainIndex <- createDataPartition(iris$Species, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+train <- dataBM[.trainIndex, ]
+test <- dataBM[-.trainIndex, ]
+
+cntrl <- trainControl(method = 'cv', number = 5)
+rfModel <- train(avg_sales ~ genre_fct + platform_fct + publisher_fct, data = train,
+                 method = 'rf', 
+                 trControl = cntrl)
+rfModel
+
+
+
+
